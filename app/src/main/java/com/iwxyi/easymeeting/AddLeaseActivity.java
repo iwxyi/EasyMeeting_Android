d14aefef9e41ce5def3118a6b3e6e5eae6fe27db
mrxy001 <wxy19980615@gmail.com>
2019-03-03 13:40:51 +0800

    新增添加租约的界面

--- app/src/main/java/com/iwxyi/easymeeting/AddLeaseActivity.java
+++ app/src/main/java/com/iwxyi/easymeeting/AddLeaseActivity.java
@@ -1,7 +1,11 @@
 package com.iwxyi.easymeeting;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class AddLeaseActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class AddLeaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_lease);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "请仔细阅读租约条款", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
